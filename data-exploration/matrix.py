0d7c1e557b334a4f798c9097d18d4ce0a1dbd94d
jww2145 <jww2145@columbia.edu>
2024-03-11 15:50:13 -0500

    Adding correlation matrix for cross-sectional

--- data-exploration/matrix.py
+++ data-exploration/matrix.py
@@ -2,12 +2,14 @@
 import pandas as pd
 import numpy as np
 import json
-from scipy.stats import chi2_contingency
 
 df = pd.read_csv('/Users/joshiebestie/Downloads/linear regression project/trade blocs ntbs.csv')
+df_toadd = pd.read_csv('/Users/joshiebestie/Downloads/trade blocs tariffs.csv')
+
+df_combined = pd.concat([df, df_toadd], ignore_index=True)
 
 
-X = df[['l_lnntb', 'lngdppc', 'wlxm', 'wlrca', 'ntbcov']]
+X = df_combined[['l_lnsmfn', 'dxr', 'imf', 'fsharec', 'wlrcac', 'polity']]
 corr_matrix = X.corr()
 
-print(corr_matrix)
+corr_matrix.to_csv('correlation_matrix.csv')
