808e332c289f00aafdab3a52aa8d759da5a457d8
jww2145 <jww2145@columbia.edu>
2024-03-16 18:34:09 -0400

    Adding a visualization of the model

--- data-exploration/ols.py
+++ data-exploration/ols.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import statsmodels.api as sm
+import plotly.graph_objects as go
+
+
+import json
 
 
 df = pd.read_csv('/Users/joshiebestie/Downloads/linear regression project/trade blocs tariffs.csv')
@@ -18,3 +22,37 @@ print(model.summary())
 model_summary = model.summary().as_csv()
 with open('model_summary.csv', 'w') as file:
     file.write(model_summary)
+
+
+
+# Create the 3D scatter plot
+fig = go.Figure(data=[go.Scatter3d(
+    x=data['l_lnsmfn'],
+    y=data['imf'],
+    z=data['polity'],
+    mode='markers',
+    marker=dict(
+        size=5,
+        color=data['dlnsmfn'],
+        colorscale='Viridis',
+        opacity=0.8
+    )
+)])
+
+# Add layout and title
+fig.update_layout(
+    scene=dict(
+        xaxis_title='Initial MFN',
+        yaxis_title='IMF',
+        zaxis_title='Polity'
+    ),
+    width=800,
+    height=600,
+    margin=dict(r=20, b=10, l=10, t=10)
+)
+
+# Export the plot as a JSON object
+plot_json = fig.to_json()
+
+with open('plot_data.json', 'w') as file:
+    file.write(plot_json)
